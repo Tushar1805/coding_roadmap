ee33e1fc34311427905891722a85c6a1364fd371
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2023-04-08 18:57:19 +0530

    Make the array beautiful

---
+++ GFG/Easy/makeBeautiful.cpp
@@ -0,0 +1,89 @@
+// https://practice.geeksforgeeks.org/problems/badefd58bace4f2ca25267ccfe0c9dc844415e90/1
+
+// Make the array beautiful
+
+// Given an array of negative and non-negative integers. 
+// You have to make the array beautiful. 
+// An array is beautiful if two adjacent integers, arr[i] and arr[i+1] are either negative or non-negative. 
+// And you can do the following operation any number of times until the array becomes beautiful.
+
+// If two adjacent integers are different i.e. one of them is negative and other is non-negative, remove them.
+// Return the beautiful array after performing the above operation.
+
+// Note:An empty array is also a beautiful array. 
+// There can be many adjacent integers which are different as stated above. 
+// So remove different adjacent integers as described above from left to right.
+
+// Example 1:
+
+// Input: 4 2 -2 1
+// Output: 4 1
+// Explanation: As at indices 1 and 2 , 2 and -2 have
+// different sign, they are removed. And the  the final
+// array is: 4 1.
+// Example 2:
+
+// Input: 2 -2 1 -1
+// Output: []
+// Explanation: As at indices 0 and 1, 2 and -2 have
+// different sign, so they are removed. Now the array
+// is 1 -1.Now 1 and -1 are also removed as they have
+// different sign. So the final array is empty. 
+// Your Task:
+// You don't need to read input or print anything. 
+// Your task is to complete the function makeBeautiful()
+// which takes an array as an input parameter and returns an array.
+
+// Expected Time Complexity: O(N)
+// Expected Space Complexity: O(N)
+
+// Constraints:
+// 1 <= size of the array <= 105
+// -105 <= arr[i] <= 105
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> makeBeautiful(vector<int> arr) {
+    stack<int> st;
+    for(auto x: arr){
+        if(st.size()){
+            if((st.top()>=0 and x< 0) or (st.top()<0 and x>=0)){
+                st.pop();
+            }else{
+                st.push(x);
+            }
+        }else{
+            st.push(x);
+        }
+    }
+    vector<int> ans;
+    while(st.size()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cout<<"Enter the size of the array: ";
+	cin>>n;
+	vector<int> arr, ans;
+	cout<<"\nEnter the "<<n<<" elements of array: ";
+	for (int i = 0; i < n; ++i)
+	{
+		int value;
+		cin>>value;
+		arr.push_back(value);
+	}
+	ans = makeBeautiful(arr);
+	cout<<"\nBeautiful Array: ";
+	for (int i = 0; i < ans.size(); ++i)
+	{
+		cout<<ans[i]<<" ";
+	}
+	return 0;
+}
