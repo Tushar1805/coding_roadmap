f7dc8cb3683bbbeb267dd91aa84e431aae2ff768
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2023-04-26 09:35:10 +0530

    Data Types

---
+++ tutorial/tutorial1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+	// int n;
+	// It gets the input
+//	scanf("%d", &n);
+//	printf("Hello World \nyour number is:");
+ 	char ch = 'a';
+ 	int a = 10;  //
+ 	float b = 5.3;
+ 	
+ 	printf("ASCII: %.2f", b);
+}
