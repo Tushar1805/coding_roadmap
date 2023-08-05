3b51ebcc33c2587ce4dcd99208d9bffbff6b21ec
Tushar Kalbhande <tusharkalbhande18@gmail.com>
2023-08-05 20:54:54 +0530

    Recursion Basics

--- prepInstaTop100/31.cpp
+++ prepInstaTop100/31.cpp
@@ -4,12 +4,12 @@
 #include<math.h>
 using namespace std;
 
-long long int getDecimal(int num){
+long long int getDecimal(int num) {
 	int i = 0;
 	int result = 0;
 
 	//converting binary to decimal
-	while(num != 0){
+	while (num != 0) {
 		int digit = num % 10;
 		result += digit * pow(2, i);
 		num /= 10;
@@ -21,9 +21,9 @@ long long int getDecimal(int num){
 int main(int argc, char const *argv[])
 {
 	long long int binary;
-	cout<<"Enter the binary number: ";
-	cin>>binary;
+	cout << "Enter the binary number: ";
+	cin >> binary;
 	// long long int res = getDecimal(binary);
-	cout<<"\n\nDecimal Conversion of "<<binary<<" is: "<<getDecimal(binary);
+	cout << "\n\nDecimal Conversion of " << binary << " is: " << getDecimal(binary);
 	return 0;
 }
--- prepInstaTop100/34.cpp
+++ prepInstaTop100/34.cpp
@@ -1,28 +1,39 @@
 // Decimal to Binary conversion
 
 #include<iostream>
+#include<math.h>
 using namespace std;
 
-void convertBinary(int dec){
-	int binary[32];
+void convertBinary(int dec) {
+	// int binary[32];
+	int result = 0;
 	int i = 0;
 
-	while(dec != 0){
-		binary[i] = dec % 2;
-		dec /= 2;
-		i++; 
-	} 
+	// Bruete Force
+	// while (dec != 0) {
+	// 	binary[i] = dec % 2;
+	// 	dec /= 2;
+	// 	i++;
+	// }
 
-	for (int j = i-1; j >= 0; j--)
-		cout<<binary[j];
+	// Optimized
+	while (dec != 0) {
+		int digit = dec & 1;
+		result += pow(10, i) * digit;
+		dec = dec >> 1;
+		i++;
+	}
+	cout << result;
+	// for (int j = i-1; j >= 0; j--)
+	// 	cout<<binary[j];
 }
 
 int main(int argc, char const *argv[])
 {
 	int dec;
-	cout<<"Enter the Decimal number: ";
-	cin>>dec;
-	cout<<"\nBinary Conversion of "<<dec<<" is: ";
+	cout << "Enter the Decimal number: ";
+	cin >> dec;
+	cout << "\nBinary Conversion of " << dec << " is: ";
 	convertBinary(dec);
 	return 0;
 }
