8d72bf8f240cd91eafc5d777affaf822078a9d31
Tushar Kalbhande <tusharkalbhande18@gmail.com>
2023-09-18 10:37:55 +0530

    Fibonacci using recursion

--- Easy Stuff/36.cpp
+++ Easy Stuff/36.cpp
@@ -1,22 +1,32 @@
 // 36. print fibonacci series up to n terms
 
-// a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. 
+// a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
 // The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 
 #include<stdio.h>
+
+// Using recursion
+
+int fib(int n) {
+	if (n <= 0) return 0;
+	if (n == 1) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
 int main(int argc, char const *argv[])
 {
 	int n, f0 = 0, f1 = 1, f2;
 	printf("Enter any positive integer (less than 25): ");
 	scanf("%d", &n);
 	printf("\nFibonacci series: ");
-	printf("%d ", f1);
+	// printf("%d ", f1);
 	for (int i = 0; i < n; ++i)
 	{
-		f2 = f0 + f1;
-    	printf("%d ", f2);
-    	f0 = f1;
-    	f1 = f2;
+		// f2 = f0 + f1;
+		// printf("%d ", f2);
+		// f0 = f1;
+		// f1 = f2;
+
+		printf("%d ", fib(i));
 	}
 	return 0;
 }
