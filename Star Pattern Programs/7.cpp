0ec67c5d17d9df763b109b822758385375438acf
Tushar Kalbhande <tusharkalbhande18@gmail.com>
2024-06-02 21:29:25 +0530

    Home pattern

--- Star Pattern Programs/7.cpp
+++ Star Pattern Programs/7.cpp
@@ -12,32 +12,32 @@ int main(int argc, char const *argv[])
 	printf("Enter the number of rows: ");
 	scanf("%d", &rows);
 	l = rows * 4;
-	n = l-4;
+	n = l - 4;
 	for (i = 0; i < rows; ++i)
 	{
 		for (j = 0; j <= i ; ++j)
 		{
 			printf("* ");
 		}
-		for (k = 0; k < (l-4) - 2; ++k)
+		for (k = 0; k < (l - 4) - 2; ++k)
 		{
 			printf(" ");
 		}
-		if (i == rows -1)
+		if (i == rows - 1)
 		{
-			for (m = k+1; m <= n - i; ++m)
+			for (m = k + 1; m <= n - i; ++m)
 			{
 				printf("* ");
 			}
-		}else{
-			for (m = k+1; m <= n - i - 1; ++m)
+		} else {
+			for (m = k + 1; m <= n - i - 1; ++m)
 			{
-					printf("* ");
+				printf("* ");
 			}
 		}
 		printf("\n");
-		l -= 4;	
-		n = n-2;	
+		l -= 4;
+		n = n - 2;
 	}
 	return 0;
 }
